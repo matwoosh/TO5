c0e98065208a7b9ac32c0e6381dd6454ef31fa94
Mateusz Kopeć <ma464ti@gmail.com>
2016-05-15 13:49:31 +0200

    Code reformatted

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Integer k;
-        ClientA[] clients = new ClientA[10];
         ProductA productA = new ProductA("Product1");
         ProductB productB = new ProductB("Product2");
+
+        ClientA[] clients = new ClientA[10];
+
+        Integer k;
         Random r = new Random(System.currentTimeMillis());
+
         for (int i = 0; i < 10; i++) {
             k = ((1 + r.nextInt(9)) * 100000000 + r.nextInt(100000000));
             clients[i] = new ClientA(k.toString());
@@ -17,6 +20,7 @@ public class Main {
                 productB.addClient(clients[i]);
             }
         }
+
         productA.notifyAllClients();
         productB.notifyAllClients();
 
