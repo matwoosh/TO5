bd32a8d19f9d2b6f351472017bc2640a3e8713d7
Jan Górski <vaniel@op.pl>
2016-05-15 12:17:21 +0200

    Takie podsatwy, mozna cos dodac.

---
+++ src/Klient.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Jan on 2016-05-15.
+ */
+public interface Klient {
+
+    public void powiadom();
+}
---
+++ src/ProduktA.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jan on 2016-05-15.
+ */
+public class ProduktA implements Produkt {
+    private List<Klient> _klient = new ArrayList<>();
+
+
+    @Override
+    public boolean dodajKlient(Klient k) {
+        if (!_klient.contains(k)) {
+            _klient.add(k);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean usunKlienta(Klient k) {
+        int index = _klient.indexOf(k);
+        if (index !=-1){
+            _klient.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void powiadomKlientow() {
+        _klient.forEach((klient) -> klient.powiadom());
+    }
+}
+
